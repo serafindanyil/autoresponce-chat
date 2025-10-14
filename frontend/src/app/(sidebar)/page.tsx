80931f94562b57aa3436ffb562381d41b4b1021e
serafindanyil <danyil.serafin.ir.2023@lpnu.ua>
2025-10-14 13:26:33 +0300

    feat: implement chat and welcome pages; update routing in sidebar to use new components

--- frontend/src/app/(sidebar)/page.tsx
+++ frontend/src/app/(sidebar)/page.tsx
@@ -1,5 +1,11 @@
-import HomePage from "@/pages/home/home-page";
+import ChatPage from "@/pages/chat/chat-page";
+import WelcomeToChat from "@/pages/welcome-to-chat/welcome-to-chat";
+
+const CHAT_MAPPER = {
+	chat: ChatPage,
+	start: WelcomeToChat,
+};
 
 export default function Home() {
-	return <HomePage />;
+	return <CHAT_MAPPER.start />;
 }
